38f7f4e79dfa18d9ceddce9ea82584f1f9eb7261
overfittingprimes <pen314paper@gmail.com>
2025-06-30 08:51:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -43,33 +43,52 @@ MEANS = {
 with st.sidebar:
     st.header("Transaction Details")
     
-    distance_home_input = st.selectbox("Distance from home address", ["None", "Custom"])
-    if distance_home_input == "Custom":
-        distance_home = st.number_input("Distance (km)", 0.0, 500.0, 37.31)
+    use_distance_home = st.checkbox("Distance from home address", value=True)
+    if use_distance_home:
+        distance_home = st.number_input("Distance (km)", 0.0, 500.0, 37.31, key="home")
     else:
         distance_home = MEANS['distance_from_home']
     
-    distance_last_input = st.selectbox("Distance from last transaction", ["None", "Custom"])
-    if distance_last_input == "Custom":
-        distance_last = st.number_input("Distance (km)", 0.0, 200.0, 7.12)
+    use_distance_last = st.checkbox("Distance from last transaction", value=True)
+    if use_distance_last:
+        distance_last = st.number_input("Distance (km)", 0.0, 200.0, 7.12, key="last")
     else:
         distance_last = MEANS['distance_from_last_transaction']
     
-    price_ratio_input = st.selectbox("Price vs typical spending", ["None", "Custom"])
-    if price_ratio_input == "Custom":
-        price_ratio = st.number_input("Ratio", 0.1, 20.0, 2.96)
+    use_price_ratio = st.checkbox("Price vs typical spending", value=True)
+    if use_price_ratio:
+        price_ratio = st.number_input("Ratio", 0.1, 20.0, 2.96, key="price")
     else:
         price_ratio = MEANS['ratio_to_median_purchase_price']
     
-    repeat_retailer = st.selectbox("Previously used retailer", ["None", "No", "Yes"])
-    used_chip = st.selectbox("Chip card used", ["None", "No", "Yes"])
-    used_pin = st.selectbox("PIN entered", ["None", "No", "Yes"])
-    online_order = st.selectbox("Online transaction", ["None", "No", "Yes"])
+    use_repeat = st.checkbox("Previously used retailer", value=True)
+    if use_repeat:
+        repeat_retailer = st.selectbox("", ["No", "Yes"], key="repeat")
+    else:
+        repeat_retailer = "Default"
+    
+    use_chip = st.checkbox("Chip card used", value=True)
+    if use_chip:
+        used_chip = st.selectbox("", ["No", "Yes"], key="chip")
+    else:
+        used_chip = "Default"
+    
+    use_pin = st.checkbox("PIN entered", value=True)
+    if use_pin:
+        used_pin = st.selectbox("", ["No", "Yes"], key="pin")
+    else:
+        used_pin = "Default"
+    
+    use_online = st.checkbox("Online transaction", value=True)
+    if use_online:
+        online_order = st.selectbox("", ["No", "Yes"], key="online")
+    else:
+        online_order = "Default"
 
-repeat_val = MEANS['repeat_retailer'] if repeat_retailer == "None" else (1 if repeat_retailer == "Yes" else 0)
-chip_val = MEANS['used_chip'] if used_chip == "None" else (1 if used_chip == "Yes" else 0)
-pin_val = MEANS['used_pin_number'] if used_pin == "None" else (1 if used_pin == "Yes" else 0)
-online_val = MEANS['online_order'] if online_order == "None" else (1 if online_order == "Yes" else 0)
+repeat_val = MEANS['repeat_retailer'] if repeat_retailer == "Default" else (1 if repeat_retailer == "Yes" else 0)
+chip_val = MEANS['used_chip'] if used_chip == "Default" else (1 if used_chip == "Yes" else 0)
+pin_val = MEANS['used_pin_number'] if used_pin == "Default" else (1 if used_pin == "Yes" else 0)
+online_val = MEANS['online_order'] if online_order == "Default" else (1 if online_order == "Yes" else 0)
 
 X = np.array([[distance_home, distance_last, price_ratio, repeat_val, chip_val, pin_val, online_val]])
 prediction = model.predict(X)[0]
@@ -78,7 +97,7 @@ probability = model.predict_proba(X)[0][1]
 fig = go.Figure(go.Indicator(
     mode="gauge+number",
     value=probability * 100,
-    title={'text': "Fraud Risk"},
+    title={'text': "Credit Card Fraud Detection"},
     gauge={
         'axis': {'range': [0, 100]},
         'bar': {'color': "#34495e"},
@@ -90,7 +109,7 @@ fig = go.Figure(go.Indicator(
         ]
     }
 ))
-fig.update_layout(height=300, margin=dict(l=0, r=0, t=30, b=0))
+fig.update_layout(height=350, margin=dict(l=0, r=0, t=60, b=0))
 st.plotly_chart(fig, use_container_width=True)
 
 if probability > 0.7:
